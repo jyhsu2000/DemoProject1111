d7551795e17d9cf24cfdaff4faad249e28f5f203
jyhsu <jyhsu2000@gmail.com>
2022-11-11 18:38:05 +0800

    Remove message

--- src/main/java/com/github/jyhsu2000/Main.java
+++ src/main/java/com/github/jyhsu2000/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi, world!");
         System.out.println("Hello Java!");
-        System.out.println("另一行訊息");
     }
 }
